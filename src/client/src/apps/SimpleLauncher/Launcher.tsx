53a1007f0521c4f0f193ec755cab38231ed564c9
Volodymyr Syzov <sizov.v.f@gmail.com>
2019-11-12 16:57:08 +0000

    fix: imporve spotlight in launcher

--- src/client/src/apps/SimpleLauncher/Launcher.tsx
+++ src/client/src/apps/SimpleLauncher/Launcher.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { rules } from 'rt-styleguide'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,11 +7,15 @@ import { appConfigs } from './applicationConfigurations'
 import { LaunchButton } from './LaunchButton'
 import { LogoIcon } from 'rt-components'
 import { createGlobalStyle } from 'styled-components'
-import { ThemeStorageSwitch, styled } from 'rt-theme'
+import { styled, ThemeStorageSwitch } from 'rt-theme'
 import { open } from './tools'
 import { getOpenFinPlatform } from 'rt-platforms'
+import { Bounds } from 'openfin/_v2/shapes'
+import SearchIcon from './icons/searchIcon'
 
 library.add(faSignOutAlt)
+
+const INPUT_HEIGHT = 40
 
 const exitHandler = async () => {
   const { OpenFin } = await getOpenFinPlatform()
@@ -36,35 +40,113 @@ const LauncherExit = () => (
     </LaunchButton>
   </ButtonContainer>
 )
-export class Launcher extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <LauncherGlobalStyle />
-        <Root>
-          <LogoContainer>
-            <LogoIcon width={1.3} height={1.3} />
-          </LogoContainer>
-          {appConfigs.map(app => (
-            <ButtonContainer key={app.name}>
-              <LaunchButton onClick={() => open(app)}>
-                {app.icon}
-                <IconTitle>{app.name}</IconTitle>
-              </LaunchButton>
-            </ButtonContainer>
-          ))}
-          <LauncherExit />
-          <ThemeSwitchContainer>
-            <ThemeStorageSwitch />
-          </ThemeSwitchContainer>
-        </Root>
-      </React.Fragment>
-    )
-  }
+
+async function setOriginalWindowSize(initialBounds: Bounds) {
+  const window = await fin.Window.getCurrent()
+  return window.animate(
+    {
+      size: {
+        duration: 200,
+        height: initialBounds.height,
+        width: initialBounds.width,
+      },
+    },
+    {
+      tween: 'ease-in-out',
+      interrupt: true,
+    },
+  )
 }
 
-const Root = styled.div`
-  height: 100%;
+async function extendWindowSize(initialBounds: Bounds) {
+  const window = await fin.Window.getCurrent()
+  // const bounds = await window.getBounds()
+  return window.animate(
+    {
+      size: {
+        duration: 200,
+        height: initialBounds.height + INPUT_HEIGHT,
+        width: initialBounds.width,
+      },
+    },
+    {
+      tween: 'ease-in-out',
+      interrupt: true,
+    },
+  )
+}
+
+const getInitialBounds = async () => {
+  const window = await fin.Window.getCurrent()
+  return window.getBounds()
+}
+
+export const Launcher: React.FC = () => {
+  const [initialBounds, setInitialBounds] = useState<Bounds>()
+  const [isSearchVisible, setIsSearchVisible] = useState(false)
+
+  const ref = useRef<() => void>()
+
+  ref.current = () => {
+    if (!isSearchVisible || !initialBounds) {
+      return
+    }
+    setIsSearchVisible(false)
+    setOriginalWindowSize(initialBounds)
+  }
+
+  useEffect(() => {
+    getInitialBounds().then(setInitialBounds)
+
+    document.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        ref.current!()
+      }
+    })
+  }, [])
+
+  const showSearch = () => {
+    if (isSearchVisible || !initialBounds) {
+      return
+    }
+    setIsSearchVisible(true)
+    extendWindowSize(initialBounds)
+  }
+
+  const Spotlight = () => (
+    <ButtonContainer>
+      <LaunchButton onClick={showSearch}>{SearchIcon}</LaunchButton>
+    </ButtonContainer>
+  )
+
+  return (
+    <React.Fragment>
+      <LauncherGlobalStyle />
+      <HorizontalContainer>
+        <LogoContainer>
+          <LogoIcon width={1.3} height={1.3} />
+        </LogoContainer>
+        <Spotlight />
+        {appConfigs.map(app => (
+          <ButtonContainer key={app.name}>
+            <LaunchButton onClick={() => open(app)}>
+              {app.icon}
+              <IconTitle>{app.name}</IconTitle>
+            </LaunchButton>
+          </ButtonContainer>
+        ))}
+        <LauncherExit />
+        <ThemeSwitchContainer>
+          <ThemeStorageSwitch />
+        </ThemeSwitchContainer>
+      </HorizontalContainer>
+      <Input />
+    </React.Fragment>
+  )
+}
+
+const HorizontalContainer = styled.div`
+  height: 52px;
   width: 100%;
   display: flex;
   align-items: center;
@@ -116,4 +198,16 @@ const LogoContainer = styled(IconContainer)`
     fill: ${({ theme }) => theme.core.textColor};
   }
   ${rules.appRegionDrag};
+`
+
+const Input = styled.input`
+  padding: 8px;
+  width: 100%;
+  height: ${INPUT_HEIGHT}px;
+  background: none;
+  outline: none;
+  border: none;
+  font-size: 1.25rem;
+  color: ${({ theme }) => theme.core.textColor};
+  ${rules.appRegionNoDrag};
 `
